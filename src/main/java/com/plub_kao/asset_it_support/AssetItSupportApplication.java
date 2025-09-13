37965628b2b462bd8fd96abeab87a820bd28c98d
vorapol <vorapolanananusit2@gmail.com>
2025-09-13 23:26:02 +0700

    Initial commit

---
+++ src/main/java/com/plub_kao/asset_it_support/AssetItSupportApplication.java
@@ -0,0 +1,15 @@
+package com.plub_kao.asset_it_support;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetItSupportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssetItSupportApplication.class, args);
+    }
+
+
+
+}
---
+++ src/test/java/com/plub_kao/asset_it_support/AssetItSupportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plub_kao.asset_it_support;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetItSupportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
